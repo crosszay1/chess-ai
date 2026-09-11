8879d3258a6f4f23ae499b4a315aeab53881b400
crosszay1 <104384163+crosszay1@users.noreply.github.com>
2026-09-11 17:00:52 +0000

    Add simple stub algorithm that moves randomly

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("123")
+import { Chess } from 'chess.js'
+
+
+// Just plays a random game of chess
+// based on https://github.com/jhlywa/chess.js example code
+
+class Algorithm {
+    public decideMove(chess: Chess) {
+        const moves = chess.moves() // Get moves
+        const move = moves[Math.floor(Math.random() * moves.length)] // decide move randomly
+        chess.move(move) // make the move
+
+    }
+}
+const chess = new Chess()
+const algorithm = new Algorithm
+
+while (!chess.isGameOver()) {
+    algorithm.decideMove(chess)
+}
+
+console.log(chess.pgn())
