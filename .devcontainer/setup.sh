760b35c0a4ca4aa118348b887982d762a1aa597f
crosszay1 <104384163+crosszay1@users.noreply.github.com>
2026-09-11 12:35:34 -0400

    Add setup script for development container
    Set up development environment with necessary tools.

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# For debian based enviroments only unfortuantly, will probably only be using them though so it's fine
+
+sudo apt install curl fish # Get curl & fish
+curl -fsSL https://tailscale.com/install.sh | sh # Install tailscale
+curl https://cursor.com/install -fsS | bash # Install cursor
