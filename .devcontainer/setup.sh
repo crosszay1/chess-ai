c6246ce5381c319291107c8f06fafc930b969852
crosszay1 <104384163+crosszay1@users.noreply.github.com>
2026-09-12 02:10:07 +0000

    Remove port forwarding, and tailscale installation from devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,4 @@
 
 # For debian based enviroments only unfortuantly, will probably only be using them though so it's fine
 
-sudo apt install curl fish # Get curl & fish
-curl -fsSL https://tailscale.com/install.sh | sh # Install tailscale
 curl https://cursor.com/install -fsS | bash # Install cursor
